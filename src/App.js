08efa6f21eaed9f9f40947f1cfa785ffe14840e8
Jacob Kayen <jacob.kayen@gmail.com>
2017-04-29 13:30:13 -0400

    new branch for routing

--- src/App.js
+++ src/App.js
@@ -13,21 +13,3 @@ export default class App extends Component{
   }
 }
 
-// import React, { Component } from 'react';
-// import { Router, Scene } from 'react-native-router-flux';
-
-// import PageOne from './pageOne';
-// import PageTwo from './pageTwo';
-
-// export default class App extends Component {
-//   render() {
-//     return (
-//       <Router hideNavBar= "true">
-//         <Scene key="root">
-//           <Scene key="pageOne" component={PageOne} title="PageOne" initial={true} />
-//           <Scene key="pageTwo" component={PageTwo} title="PageTwo" />
-//         </Scene>
-//       </Router>
-//     )
-//   }
-// }
